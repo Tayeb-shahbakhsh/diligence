35747ed5c33f30b1c944c5f6c48f8b07083d9af3
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-02-24 00:41:24 +0330

    progress bar working

--- app/src/main/java/com/example/diligence/FirstFragment.kt
+++ app/src/main/java/com/example/diligence/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.example.diligence
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,33 +9,34 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.diligence.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val MOCK_TIMOUT : Long = 2 * 60000
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        binding.progressBar.max = MOCK_TIMOUT.toInt()
+
+        object: CountDownTimer(MOCK_TIMOUT, 10){
+            override fun onTick(millisUntilFinished: Long) {
+                binding.progressBar.progress = millisUntilFinished.toInt()
+            }
+
+            override fun onFinish() {
+                binding.progressBar.progress = 1000
+            }
+        }.start()
     }
 
     override fun onDestroyView() {
